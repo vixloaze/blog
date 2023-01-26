bdde43c454ad14082104227a1b8429ad62750368
조성현 <agllwy116@naver.com>
2023-01-26 19:23:53 +0900

    yml 설정 및 파일 리턴 관련 코드

---
+++ src/main/java/com/cos/blog/test/TempControllerTest.java
@@ -0,0 +1,20 @@
+package com.cos.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TempControllerTest {
+
+    //http://localhost:8080/blog/temp/home
+    @GetMapping("/temp/home")
+    public String tempHome() {
+        System.out.println("tempHome()");
+        return "/home.html";
+    }
+
+    @GetMapping("/temp/jsp")
+    public String tempJsp() {
+        return "test";
+    }
+}
