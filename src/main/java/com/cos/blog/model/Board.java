f9d3799e4162405f3b2fcfca8f4ba8c7b297d617
조성현 <agllwy116@naver.com>
2023-01-27 02:51:09 +0900

    User 테이블, Board 테이블 생성

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,39 @@
+package com.cos.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title; // 제목
+
+    @Lob // 대용량 데이터
+    private String content; // 내용 섬머노트 라이브러리를 사용할 예정 <html>태그가 섞여서 디자인이 됨
+
+    @ColumnDefault("0")
+    private int count; // 조회수
+
+    @ManyToOne // Board = Many , user = One 여러개의 게시글은 한 명에 의해서 작성이 될 수 있다. (연관 관계)
+    @JoinColumn(name="userId")
+    private User user; // DB는 오브젝트를 저장할 수 없으나 FK,자바는 오브젝트를 저장할 수 있다.
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,39 @@
+package com.cos.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // 빌더 패턴!!
+@Entity // User 클래스가 MySQL에 테이블이 생성이 된다.
+public class User {
+
+    @Id // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+    private int id;
+
+    @Column(nullable = false, length = 30)
+    private String username; // 아이디
+
+    @Column(nullable = false, length = 100) // 암호화한 비번을 DB에 넣을거라 넉넉히 줌
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("'user'")
+    private String role; // Enum을 쓰는 것이 좋다. //admin, user, manager 권한 관련
+
+    @CreationTimestamp // 시간이 자동으로 입력이 됩니다.
+    private Timestamp createDate;
+
+}
